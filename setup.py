baebc03a648d1ad1d7beac56c4ce63ab1fbbaa49
Lewis Collard <lewis.collard@onespacemedia.com>
2019-07-18 14:56:00 +0100

    PEP8-ify setup.py.

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from distutils.core import setup
 
 setup(
-  name = 'osm-jet',
-  version = '0.0.3',
-  license='agpl-3.0',
-  description = 'A CMS skin for Django. An extension of django-jet with some fixes and additional features',
-  author = 'Onespacemedia',
-  author_email = 'developers@onespacemedia.com',
-  url = 'https://github.com/onespacemedia/osm_jet',
-  download_url = 'https://github.com/onespacemedia/osm_jet/archive/v_01.tar.gz',
-  install_requires=[
-          'Django',
-          'django-jet',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Framework :: Django :: 1.11',
-    'License :: OSI Approved :: GNU Affero General Public License v3.0',
-    'Natural Language :: English',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='osm-jet',
+    version='0.0.3',
+    license='agpl-3.0',
+    description='A CMS skin for Django. An extension of django-jet with some fixes and additional features',
+    author='Onespacemedia',
+    author_email='developers@onespacemedia.com',
+    url='https://github.com/onespacemedia/osm_jet',
+    download_url='https://github.com/onespacemedia/osm_jet/archive/v_01.tar.gz',
+    install_requires=[
+        'Django',
+        'django-jet',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Framework :: Django :: 1.11',
+        'License :: OSI Approved :: GNU Affero General Public License v3.0',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
