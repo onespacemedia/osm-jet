762a121bbce0bdf42e384f64881f5d14699ea94d
Aidan Currah <aidancurrah@gmail.com>
2019-10-14 14:22:49 +0100

    [Fix #31] Don't count the hidden form when updating management form TOTAL_FORMS

--- osm_jet/static/jet/js/src/features/compact-inline.js
+++ osm_jet/static/jet/js/src/features/compact-inline.js
@@ -57,7 +57,7 @@ CompactInline.prototype = {
     updateTotalForms: function($inline) {
         var $totalFormsInput = $inline.find('[name="' + this.prefix + '-TOTAL_FORMS"]');
         var $maxFormsInput = $inline.find('[name="' + this.prefix + '-MAX_NUM_FORMS"]');
-        var totalForms = parseInt($inline.find('.inline-related').length);
+        var totalForms = parseInt($inline.find('.inline-related:not(.empty-form)').length);
         var maxForms = $maxFormsInput.val() ? parseInt($maxFormsInput.val()) : Infinity;
 
         $totalFormsInput.val(totalForms);
